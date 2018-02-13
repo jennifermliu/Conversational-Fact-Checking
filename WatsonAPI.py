e1bf9e9e593c8fe574b628c10e22a569d959b9c9
jennifermliu <jenniferliu2018@u.northwestern.edu>
2018-02-13 12:08:06 -0600

    generate a list of keywords from watson api results

--- WatsonAPI.py
+++ WatsonAPI.py
@@ -4,6 +4,7 @@ from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions, KeywordsOptions, CategoriesOptions, ConceptsOptions, RelationsOptions, SemanticRolesOptions
 
 
+
 data=''
 with open ("oneminutetext.txt", "r") as myfile:
     data=myfile.read()
@@ -11,26 +12,46 @@ with open ("oneminutetext.txt", "r") as myfile:
 
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
-  username='dcc4c5d9-682d-4eb7-8360-fe8d010b84de',
-  password='PobFBTuJ6nEm',
-  version='2017-02-27')
+    username='dcc4c5d9-682d-4eb7-8360-fe8d010b84de',
+    password='PobFBTuJ6nEm',
+    version='2017-02-27')
 
 response = natural_language_understanding.analyze(
+    text=data,
+    features=Features(concepts=ConceptsOptions(limit=8),
+    keywords=KeywordsOptions(limit=50),
+    entities=EntitiesOptions(model='en-news',limit=50),
+    semantic_roles=SemanticRolesOptions (entities=True,keywords=True,limit=50)),
+    return_analyzed_text=True)
 
+# print(json.dumps(response, indent=2))
 
-  text=data,
-  features=Features(
-    concepts=ConceptsOptions(
-    limit=8),
-    keywords=KeywordsOptions(
-    limit=50),
-    entities=EntitiesOptions(
-      model='en-news',
-      limit=50),
-    semantic_roles=SemanticRolesOptions(
-        entities=True,
-        keywords=True,
-        limit=50)),
-  return_analyzed_text=True)
+target=[]
+sem=response['semantic_roles']
+# print(sem)
+# print(type(sem))
+# print(len(sem))
+for i in range (len(sem)):
+    # action
+    candidate=sem[i]['action']['text']
+    target.append(str(candidate))
 
-print(json.dumps(response, indent=2))
+    # object
+    objdict=sem[i]['object']
+    for key,value in objdict.iteritems():
+        # print(key)
+        if(key=='keywords'):
+            for j in range (len(value)):
+                candidate=value[j]['text']
+                target.append(str(candidate))
+
+    # subject
+    subdict=sem[i]['subject']
+    for key,value in subdict.iteritems():
+        # print(key)
+        if(key=='keywords'):
+            for j in range (len(value)):
+                candidate=value[j]['text']
+                target.append(str(candidate))
+
+print(target)
