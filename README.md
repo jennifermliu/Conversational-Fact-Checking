58841eb946ee1f6e4532ddf8112253153a0808fa
Carlos Belardi <carlosbelardi2019@u.northwestern.edu>
2018-02-15 09:47:28 -0600

    Save words from txt file into JS array

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Conversational-Fact-Checking
+# Conversational-Fact-Checking\
+
+GoogleScript.js: get one-minute text
+
+PastMinute.txt: one-minute text
+
+AnotherMinute.txt: alternative one-minute text
+
+statement.txt: simple example to test semantic analysis 
+
+SemanticAnalysis.py: get the one-minute text, run the Watson API for get semantic analysis, find keywords and send to text file as input for Politifact API
+
+testing.txt: keywords for Politifact API
+
+factchecking.html: access the Politifact API by constructing an url from keywords 
+
+helper.js: just in case we need a separated js file
+
+
+
---
+++ SemanticAnalysis.py
@@ -0,0 +1,68 @@
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+  import Features, EntitiesOptions, KeywordsOptions, CategoriesOptions, ConceptsOptions, RelationsOptions, SemanticRolesOptions
+
+
+
+data=''
+with open ("AnotherMinute.txt", "r") as myfile:
+    data=myfile.read()
+    # print(data)
+
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    username='dcc4c5d9-682d-4eb7-8360-fe8d010b84de',
+    password='PobFBTuJ6nEm',
+    version='2017-02-27')
+
+response = natural_language_understanding.analyze(
+    text=data,
+    features=Features(concepts=ConceptsOptions(limit=8),
+    keywords=KeywordsOptions(limit=50),
+    entities=EntitiesOptions(model='en-news',limit=50),
+    semantic_roles=SemanticRolesOptions (entities=True,keywords=True,limit=50)),
+    return_analyzed_text=True)
+
+# print(json.dumps(response, indent=2))
+
+target=[]
+sem=response['semantic_roles']
+# print(sem)
+# print(type(sem))
+# print(len(sem))
+for i in range (len(sem)):
+    record=sem[i]
+
+    # action
+    if 'action' in record:
+        candidate=record['action']['text']
+        target.append(str(candidate))
+
+    # object
+    if 'object' in record:
+        objdict=record['object']
+        for key,value in objdict.iteritems():
+            # print(key)
+            if(key=='keywords'):
+                for j in range (len(value)):
+                    candidate=value[j]['text']
+                    target.append(str(candidate))
+
+    # subject
+    if 'subject' in record:
+        subdict=record['subject']
+        for key,value in subdict.iteritems():
+            # print(key)
+            if(key=='keywords'):
+                for j in range (len(value)):
+                    candidate=value[j]['text']
+                    target.append(str(candidate))
+
+print(target)
+
+
+f = open('testing.txt','a')
+for i in range (len(target)):
+    f.write(target[i]+'\n')
+f.close()
